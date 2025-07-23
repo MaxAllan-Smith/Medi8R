e1ecc6844e2051c9c9794a59503e2e9419c9cda5
Max Allan-Smith <max.allan-smith@mit-tech.co.uk>
2025-07-23 18:26:17 +0100

    Add Medi8R service registration and project updates
    - Updated `Medi8R.Library.csproj` to include "Extensions" and "Services" folders, and added a package reference for `Microsoft.Extensions.DependencyInjection.Abstractions` v9.0.7.
    - Modified `Medi8R.sln.DotSettings` to add a new boolean setting for user dictionary entry indexing.
    - Created `ServiceCollectionExtensions.cs` with an `AddMedi8R` method to register `IMediator` and `Mediator` services, and to scan for types implementing `IRequestHandler<,>` and `INotificationHandler<>`.

---
+++ Medi8R.Library/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Medi8R.Library.Interfaces;
+using Medi8R.Library.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Medi8R.Library.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMedi8R(this IServiceCollection services, Assembly assembly)
+        {
+            services.AddSingleton<IMediator, Mediator>(sp =>
+                new Mediator(sp.GetRequiredService));
+
+            IEnumerable<Type> types = assembly.GetTypes()
+                .Where(t => !t.IsAbstract && !t.IsInterface);
+
+            foreach (Type type in types)
+            {
+                foreach (Type iface in type.GetInterfaces())
+                {
+                    if (!iface.IsGenericType)
+                    {
+                        continue;
+                    }
+
+                    Type genericDef = iface.GetGenericTypeDefinition();
+
+                    if (genericDef == typeof(IRequestHandler<,>) ||
+                        genericDef == typeof(INotificationHandler<>))
+                    {
+                        services.AddTransient(iface, type);
+                    }
+                }
+            }
+
+            return services;
+        }
+    }
+}
